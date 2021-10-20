b35d606081d4d2298608f731c52abeeafdce7641
msh1273 <mch1273@naver.com>
2021-10-20 16:47:15 +0900

    후위표기식

--- Data_Structure/sol1918.py
+++ Data_Structure/sol1918.py
@@ -11,17 +11,24 @@ priority = {
 operand = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 stack = []
 result = []
+ans = ''
 for i in exp:
-    if i in operand:
+    if i in operand:    #피 연산자인 경우
         result.append(i)
     elif i == '(':
         stack.append(i)
     elif i == ')':
         while stack and stack[-1] != '(':
             result.append(stack.pop())
-        stack.pop()
-    else:
+        stack.pop() # (을 빼는 역할
+    else:   # 연산자인 경우
         while stack and priority[i] <= priority[stack[-1]]:
             result.append(stack.pop())
         stack.append(i)
-print(stack)
+while stack:
+    result.append(stack.pop())
+
+for i in result:
+    ans += i
+    
+print(ans)
