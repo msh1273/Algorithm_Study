088ee746c7bd3a3d362da823d1642acfcbc14867
msh1273 <mch1273@naver.com>
2021-10-25 01:28:14 +0900

    설탕 배달

---
+++ DynamicProgramming/sol2839.py
@@ -0,0 +1,20 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+count = 0
+
+while n>0:
+    if n % 5 == 0:
+        n -= 5
+        count += 1
+    elif n % 3 == 0:
+        n -= 3
+        count += 1
+    elif  n > 5:
+        n -= 5
+        count += 1
+    else:
+        count = -1
+        break
+
+print(count)
