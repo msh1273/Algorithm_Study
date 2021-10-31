e036c58fe8ba4318273659565bf6e57d0c2eab3b
msh1273 <mch1273@naver.com>
2021-10-31 23:54:44 +0900

    런타임 에러?

--- DynamicProgramming/sol9251.py
+++ DynamicProgramming/sol9251.py
@@ -2,13 +2,14 @@ import sys
 
 a = sys.stdin.readline().rstrip()
 b = sys.stdin.readline().rstrip()
-a1 = list(a)
-b1 = list(b)
-result = []
-for i in a1:
-    for j in range(len(b1)):
-        if i == b1[j] :
-            result.append(i)
-            b1 = b1[j+1:]
-            break
-print(result)
+
+LCS = [[0]*(len(a)+1) for _ in range(len(b)+1)]
+for i in range(1,len(a)+1):
+    for j in range(1, len(b)+1):
+        if a[i-1] == b[j-1]:
+            LCS[i][j] = LCS[i-1][j-1]+1
+        else:
+            LCS[i][j] = max(LCS[i-1][j], LCS[i][j-1])
+
+print(a[0],b)
+print(LCS[-1][-1])
