0f16d752f10b294abc8330f4c7d0c5f6ba26da46
msh1273 <mch1273@naver.com>
2021-11-02 02:29:55 +0900

    LCS

--- DynamicProgramming/sol9251.py
+++ DynamicProgramming/sol9251.py
@@ -3,13 +3,14 @@ import sys
 a = sys.stdin.readline().rstrip()
 b = sys.stdin.readline().rstrip()
 
-LCS = [[0]*(len(a)+1) for _ in range(len(b)+1)]
-for i in range(1,len(a)+1):
-    for j in range(1, len(b)+1):
-        if a[i-1] == b[j-1]:
+LCS = [[0]*(len(b)+1) for _ in range(len(a)+1)]
+for i in range(0,len(a)+1):
+    for j in range(0, len(b)+1):
+        if i == 0 or j == 0:
+            LCS[i][j] == 0
+        elif a[i-1] == b[j-1]:
             LCS[i][j] = LCS[i-1][j-1]+1
         else:
             LCS[i][j] = max(LCS[i-1][j], LCS[i][j-1])
 
-print(a[0],b)
 print(LCS[-1][-1])
