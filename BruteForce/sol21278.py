dcc2d4cdb66f283786996bd4260a644f113dd6b6
msh1273 <mch1273@naver.com>
2021-12-01 23:43:00 +0900

    호석이 두마리 치킨(미구현)

--- BruteForce/sol21278.py
+++ BruteForce/sol21278.py
@@ -4,4 +4,16 @@ input = sys.stdin.readline().rstrip().split()
 # 건물의 개수 n, 도로의 개수 m
 n, m = map(int, input)
 
-print(n, m)
+building = [i for i in range(n+1)]
+
+print(building)
+chicken1 = 0
+chicken2 = 0
+
+_map = [[0 for _ in range(n)] for _ in range(n)]
+
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    _map[a-1][b-1] = _map[b-1][a-1] = 1
+
+print(_map)
