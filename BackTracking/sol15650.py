51a9f82dc36505bc9f7e70e7674257b5c1c06192
msh1273 <mch1273@naver.com>
2021-12-13 20:56:55 +0900

    N과 M(2)

---
+++ BackTracking/sol15650.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().rstrip().split())
+
+ans = []
+def bt(num):
+    if len(ans) == m:
+        print(' '.join(map(str, ans)))
+        return
+    for i in range(num, n+1):
+        if i not in ans:
+            ans.append(i)
+            bt(i+1)
+            ans.pop()
+bt(1)
