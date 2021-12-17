32f5fab71c74babe7bfd5ae751392cc18f5ce1bb
msh1273 <mch1273@naver.com>
2021-12-17 16:58:28 +0900

    외판원 순회2

--- sol10971.py
+++ sol10971.py
@@ -1,28 +1,30 @@
 import sys
 
-def dfs(node, dist, cnt):
-    global N, weights, visited, shortest
+def dfs(city, val, count):
+    global n, graph, visited, path
     
     # N개 노드 모두 방문했으면 시작점으로 되돌아가기
-    if cnt == N:
-        if weights[node][0] > 0:
-            shortest = min(shortest, dist + weights[node][0])
+    if count == n:
+        if graph[city][0] > 0:
+            path = min(path, val + graph[city][0])
         return
-    # 이웃한 노드 중 방문하지 않은 노드 방문
-    for idx, w in enumerate(weights[node]):
-        if w > 0 and not visited[idx]:
-            visited[idx] = 1
-            dfs(idx, dist+w, cnt+1)
-            visited[idx] = 0
+        
+    for i, w in enumerate(graph[city]):
+        # 도로가 있고, 방문하지 않은 도시인 경우
+        if w > 0 and not visited[i]:
+            visited[i] = 1
+            dfs(i, val+w, count+1)
+            visited[i] = 0
 
 if __name__ == '__main__':
-    N = int(input())
-    weights = []
-    visited = [0 for _ in range(N)]
-    shortest = float('inf')
-    for _ in range(N):
-        weights.append(list(map(int, input().split())))
+    n = int(sys.stdin.readline().rstrip())
+    graph = []
+    visited = [0] * n
+    path = float('inf')
+
+    for _ in range(n):
+        graph.append(list(map(int, sys.stdin.readline().rstrip().split())))
 
     visited[0] = 1
     dfs(0, 0, 1)
-    print(shortest)
+    print(path)
