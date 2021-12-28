d10059763e42c904390bbed56cc43b0c30ee0745
msh1273 <msh1273@gmail.com>
2021-12-28 23:45:02 +0900

    N-Queen 정리
    주석 추가

--- BackTracking/sol9663.py
+++ BackTracking/sol9663.py
@@ -1,13 +1,13 @@
 
 n = int(input())
 
-row = [0] * n
+row = [250] * n
 answer = 0
 
-#현재 레벨에서 놓을 수 있는지 없는지 체크하는 함수 (행, 열, 대각선)
+#현재 레벨에서 놓을 수 있는지 없는지 체크하는 함수 (행[자동 생략], 열, 대각선)
 def can(level):
     for i in range(level):
-        if row[level] == row[i] or abs(row[level] - row[i]) == level - i:
+        if row[level] == row[i] or abs(row[level] - row[i]) == level - i:   #열위치가 같은 조건 | 열위치 차이와, 행위치 차이가 같다면 대각선
             return False
     return True
 
@@ -19,8 +19,8 @@ def dfs(level):
         answer += 1
         return
     else:
-        for i in range(n):
-            row[level] = i
+        for i in range(n):  #퀸을 배치할 열 
+            row[level] = i  #퀸을 level행 i열에 배치
             if can(level):
                 dfs(level + 1)
 
