0c1609900f0c602eed0f2df1473d794d4490b07b
msh1273 <mch1273@naver.com>
2022-01-06 01:23:18 +0900

    주석 추가

--- FloydWarshall/sol1389.py
+++ FloydWarshall/sol1389.py
@@ -4,6 +4,8 @@ import sys
 N, M = map(int, sys.stdin.readline().rstrip().split())
 
 rel = [[0] * (N) for _ in range(N)]
+
+# 그래프 만들기(자기 자신에게 가는 경우는 모두 0 나머지는 inf
 for i in range(N):
     for j in range(N):
         if i == j:
@@ -11,16 +13,19 @@ for i in range(N):
         else:
             rel[i][j] = float('inf')
 
+# 경로가 이어진 곳 1로 만드는 작업
 for i in range(M):
     a, b = map(int, sys.stdin.readline().rstrip().split())
     rel[a-1][b-1] = rel[b-1][a-1] = 1
 
+
+# 플로이드 와샬
 for k in range(N):
-    score = 0
     for i in range(N):
         for j in range(N):
             rel[i][j] = min(rel[i][j], rel[i][k]+rel[k][j])
 
+# dict로 만들어서 value로 오름차순 정렬하기
 answer = []
 for i in rel:
     answer.append(sum(i))
