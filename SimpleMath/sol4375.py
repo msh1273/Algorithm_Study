1e9ca214f0cb22d639b151bb42ed3651d806d41b
msh1273 <mch1273@naver.com>
2022-01-23 23:34:40 +0900

    1

---
+++ SimpleMath/sol4375.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        n = int(input())
+        ans = "1"
+        while True:
+            if int(ans) % n == 0:
+                print(len(ans))
+                break
+            ans += "1"
+    except EOFError:
+        break
